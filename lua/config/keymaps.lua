cfd277ea9803a64fd560078a28526113e9e49ba3
João Iglésias Charters de Azevedo <joaochartersazevedo@tecnico.ulisboa.pt>
2026-04-04 01:27:59 +0100

    scroll distance, vertical split

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Open file under cursor in a vertical split (override default gf behavior)
+map("n", "gf", function()
+  local filepath = vim.fn.expand("<cfile>")
+  vim.cmd("vsplit " .. filepath)
+end, { desc = "Open file under cursor in vertical split" })
